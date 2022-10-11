74ecdff3aab7ec762dad2ab53d3699c59d29ce5e
Savva Mishin <irafforty@yandex.ru>
2022-10-11 20:22:24 +0500

    fix autotest 4 try

--- controllers/users.js
+++ controllers/users.js
@@ -31,14 +31,17 @@ module.exports.getUserById = (req, res, next) => {
 
 module.exports.postUser = (req, res, next) => {
   const {
-    name, about, avatar, email,
+    name, about, avatar, email, password,
   } = req.body;
-  bcrypt.hash(req.body.password, 10)
+  // if (!email || !password) {
+  //   throw new BadRequestError400('Неправильные почта или пароль.')
+  // }
+  bcrypt.hash(password, 10)
     .then((hash) => User.create({
       name, about, avatar, email, password: hash,
     }))
     .then(() => {
-      res.status(200).send({
+      res.status(201).send({
         data: {
           name, about, avatar, email,
         },
