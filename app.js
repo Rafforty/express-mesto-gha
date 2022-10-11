0bb1ae05307e8a876a3b97453044a228d7f961f5
Savva Mishin <irafforty@yandex.ru>
2022-10-11 21:50:09 +0500

    fix

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
-app.use((req, res, next) => {
-  next();
-});
-
 app.post('/signin', validationSignIn, login);
 app.post('/signup', validationSignUp, createUser);
 
